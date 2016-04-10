af4af6b4f9fa9e7b32745549d636aa86cb94e4d5
sunoru sidw <s@sunoru.com>
2016-04-10 19:05:49 +0800

    Change file names for standardization.

--- src/dictsupport.jl
+++
@@ -1,6 +0,0 @@
-# support function
-
-# This is defined in Base, but probably not meant for external consumption,
-# so it's redefined here.
-_tablesz(x::Integer) = x < 16 ? 16 : one(x)<<((sizeof(x)<<3)-leading_zeros(x-1))
-
