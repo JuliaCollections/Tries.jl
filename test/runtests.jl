20a51d006f7cbe4a43fc1b7a875139b6c86c16c6
rofinn <rory.finnegan@gmail.com>
2018-02-27 15:53:16 -0600

    Cleanup src/Tries.jl and test/runtests.jl w/ tests passing.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,31 +1,10 @@
 using Compat.Test
 using Tries
-const IntSet = Tries.IntSet
-using Primes, Compat
-using Compat.Unicode: lowercase
 
-@test isempty(detect_ambiguities(Base, Core, Tries))
+import Compat: Nothing
 
-tests = ["int_set",
-         "deque",
-         "circ_deque",
-         "sorted_containers",
-         "stack_and_queue",
-         "accumulator",
-         "classified_collections",
-         "disjoint_set",
-         "binheap",
-         "mutable_binheap",
-         "default_dict",
-         "ordered_dict",
-         "ordered_set",
-         "trie",
-         "list",
-         "multi_dict",
-         "circular_buffer",
-         "sorting",
-         "priority_queue"
-        ]
+
+tests = ["trie"]
 
 if length(ARGS) > 0
     tests = ARGS
